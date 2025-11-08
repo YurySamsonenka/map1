f9da78a3f40768992e1e7075fbfa0b1be577be9e
YurySamsonenka <cant09y@gmail.com>
2025-11-08 20:34:18 +0300

    feat: add YMapControlButton

--- script.js
+++ script.js
@@ -7,21 +7,24 @@ ymaps3.ready.then(() => {
 
 await ymaps3.ready;
 
-const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker, YMapControls } = ymaps3;
+const {
+  YMap,
+  YMapDefaultSchemeLayer,
+  YMapDefaultFeaturesLayer,
+  YMapMarker,
+  YMapControls,
+  YMapControlButton
+} = ymaps3;
 
-// Импортируем UI-тему с контролами
 const { YMapRotateTiltControl } = await ymaps3.import('@yandex/ymaps3-default-ui-theme');
 
 const center_coords = [37.617644, 55.755819];
 const deg_to_rad = Math.PI / 180;
 
 const map = new YMap(document.getElementById("map"), {
-  location: {
-    center: center_coords,
-    zoom: 17
-  },
+  location: { center: center_coords, zoom: 17 },
   mode: "vector",
-  behaviors: ['drag', 'scrollZoom', 'dblClick', 'mouseTilt', 'mouseRotate']
+  behaviors: ["drag", "scrollZoom", "dblClick", "mouseTilt", "mouseRotate"]
 });
 
 map.addChild(new YMapDefaultSchemeLayer());
@@ -30,12 +33,26 @@ map.addChild(new YMapDefaultFeaturesLayer());
 const marker = new YMapMarker({ coordinates: center_coords }, document.createTextNode("🚗"));
 map.addChild(marker);
 
-// Добавляем только стандартный контрол вращения/наклона на карту
-map.addChild(
-  new YMapControls({ position: 'right' }, [
-    new YMapRotateTiltControl({})
-  ])
-);
+const svg = document.createElement("div");
+svg.classList.add('block')
+svg.innerHTML = `
+  <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2.385 12.42l16.01-7.614a.6.6 0 0 1 .8.8l-7.616 16.009a.6.6 0 0 1-1.11-.068l-2.005-6.012-6.01-2.003a.6.6 0 0 1-.069-1.111z" fill="currentColor"></path></svg>
+`;
+
+const focusButton = new YMapControlButton({
+  title: "Центрировать",
+  element: svg,
+  onClick: () => {
+    map.update({
+      location: { center: center_coords, duration: 600 }
+    });
+  }
+});
+
+const controls = new YMapControls({ position: "right" });
+controls.addChild(focusButton);
+controls.addChild(new YMapRotateTiltControl({}));
+map.addChild(controls);
 
 map.update({
   camera: {
@@ -44,7 +61,6 @@ map.update({
   }
 });
 
-// ===== Управление боковой панелью =====
 const sidebar = document.getElementById("sidebar");
 const sidebarTrigger = document.getElementById("sidebar-trigger");
 const closeSidebar = document.getElementById("close-sidebar");
